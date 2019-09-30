21afb81d9c8690db1839305a967648f28a341054
André Wendler <andre.wendler@gmail.com>
2019-09-30 17:08:40 +0200

    multi-threading requests

--- bbl-xml-grab.py
+++ bbl-xml-grab.py
@@ -1,71 +1,53 @@
-from urllib.request import urlopen as uReq
+import requests
 from bs4 import BeautifulSoup as soup
+import math
+import concurrent.futures
+import re
+import time
 
-base_url = "http://digital.slub-dresden.de/oai/?verb=ListRecords&metadataPrefix=mets&set=ldp-boersenblatt"
-weiter = False
+start = time.perf_counter()
 
-#erstanfrage
-oia_raw = uReq(base_url)
-suche_xml = oia_raw.read()
-oia_raw.close()
-xml_soup = soup(suche_xml, "xml")
 
-#ergebnisse der ersten suche werden in output-file geschrieben
-for record in xml_soup.find_all("record"):
-    slub_id = record.find("slub:id").string
-    with open("./" + slub_id + ".mets", mode='a+') as file:
-        file.write(record.prettify())
-  
-resumption_token = xml_soup.find('resumptionToken').string
+base_url = 'https://digital.slub-dresden.de/oai/'
+recordsearch_term = {'verb': 'GetRecord', 'metadataPrefix': 'mets'}
+#listsearch_term = {'verb': 'ListIdentifiers', 'metadataPrefix': 'mets', 'set': 'ldp-boersenblatt'}
+listsearch_term = {'verb': 'ListIdentifiers', 'metadataPrefix': 'mets', 'set': 'becher-institute-exam-paper'}
 
-if resumption_token:
-    weiter = True
+def download_xml(params):
+    response = requests.get(base_url, params=params)
+    output_soup = soup(response.content, "lxml")
+    return output_soup
 
-while weiter == True:
-    oia_raw = uReq("http://digital.slub-dresden.de/oai/?verb=ListRecords&resumptionToken=" + resumption_token)
-    suche_xml = oia_raw.read()
-    oia_raw.close()
-    xml_soup_new = soup(suche_xml, "xml")
+def download_record(id):
+    output_soup = download_xml({'verb': 'GetRecord', 'metadataPrefix': 'mets', 'identifier': id})
+        
+    #slub_id = output_soup.find("slub:id").get_text().strip()
+    #slub_id = re.search('\d{8}X.+',id).group()
+    slub_id = re.search('id-.+',id).group()
+    with open(f'./test/{slub_id}.mets', mode='w', encoding='utf-8') as f:
+        f.write(output_soup.prettify())
 
-    new_token = xml_soup_new.find('resumptionToken').string
+#erstanfrage um zu ermitteln, wieviele ergebnisse die suche im ganzen hat
+xml_soup = download_xml(listsearch_term)
 
-    if new_token:
-        weiter = True
-    else:
-        weiter = False
+suchlaeufe = math.ceil(int(xml_soup.resumptiontoken['completelistsize']) // 25) + 1
+print(suchlaeufe, 'Suchläufe')
 
-    cursor = int(xml_soup_new.resumptionToken['cursor'])
-    print (cursor)    
+for i in range(suchlaeufe):
+    xml_soup_new = download_xml(listsearch_term) if i == 0 else download_xml({'verb': 'ListIdentifiers','resumptionToken': resumption_token})
 
-    for record in xml_soup_new.find_all("record"):
-        slub_id = record.find("slub:id").string
-        with open("./" + slub_id + ".mets", mode='a+') as file:
-            file.write(record.prettify())
+    id_liste = [id.get_text().strip() for id in xml_soup_new.find_all('identifier')]
+    #threads über diese liste laufen lassen
+
+    with concurrent.futures.ThreaPoolExecutor() as executor:
+        results = executor.map(download_record, id_liste)
+
+    new_token = xml_soup_new.find('resumptiontoken').get_text()
+    print('neuer cursor ', xml_soup_new.resumptiontoken['cursor'])
  
     resumption_token = new_token
-    print (resumption_token)
+    print ('neuer token=', resumption_token)
 
+finish = time.perf_counter()
 
-
-
-
-
-
-
-#complete_list_size = int(xml_soup.resumptionToken['completeListSize'])
-#cursor = int(xml_soup.resumptionToken['cursor'])
-
-
-
-# for record in xml_soup.ListRecords:
-#     seiten = len(record.find("mets:fileGrp", USE='MIN'))
-#     slub_id = record.find("slub:id", limit=1).string
-
-
-
-
-
-
-# ######
-# record.metadata.mets:mets.mets:fileSec.mets:fileGrp USE="MIN".mets:file
-# return len(soup.find_all(tag_name))
+print(f'Finished in {round(finish-start, 2)} second(s)')
